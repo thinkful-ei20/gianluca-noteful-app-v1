e52bb432782e7e09a6dab12370e991780f51c98b
patersog <gianluca.paterson@gmail.com>
2018-04-16 16:31:19 -0400

    Add end points

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-'use strict';
 
+
+const express = require('express');
 const data = require('./db/notes');
+const app = express();
 
-console.log('Hello Noteful!');
+app.use(express.static('public'));
 
-// INSERT EXPRESS APP CODE HERE...
+app.get('/api/notes', (req, res) => {
+	req.query.searchTerm ? res.json(data.filter(note => note.title.includes(req.query.searchTerm))) : res.json(data);
+	//MENTOR SESSION
+});
+
+app.get('/api/notes/:id', (req, res) => {
+	res.json(data.find(item => item.id === Number(req.params.id)));
+});
+
+app.listen(8080, function() {
+	console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+	console.error(err);
+});
